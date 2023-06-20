8221aca67c8f1794de9dd2e3b6fbc091008b80fc
Thomas the Maker <nnt0001@uah.edu>
2023-06-19 20:51:48 -0400

    Hello! I'm back
    After the official release, I took a break to do a few things:
    - Prepare the publishing
    - Prepare the website
    - Prepare some rebranding
    
    Now, I'm back with more focus:
    VNC mode & better terminal experience coming

--- README.md
+++ README.md
@@ -17,3 +17,7 @@ and make mainapp a stateful widget, and run memorycheck on init
 
 Bug #2:
 List tiles do not show up on first boot (after closing app), but only when hot reload on Android Studio or setState on app
+
+Further improvement:
+Making text copyable
+![img_2.png](img_2.png)
--- build/app/generated/source/buildConfig/debug/com/example/comfyssh/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/example/comfyssh/BuildConfig.java
@@ -5,8 +5,8 @@ package com.example.comfyssh;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.comfyssh";
+  public static final String APPLICATION_ID = "tech.comfystudio.comfyssh";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0.0";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.1.0";
 }
