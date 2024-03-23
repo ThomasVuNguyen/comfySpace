62f21309d008431d732e0c7c02ee4e117d04cdc8
Thomas Nguyen <tungvunguyennguyen@gmail.com>
2024-03-23 13:29:25 -0400

    what if buttons can look better?

--- v2_1/lib/project_space_screen/button_list/button_global/button_sort.dart
+++ v2_1/lib/project_space_screen/button_list/button_global/button_sort.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:v2_1/home_screen/comfy_user_information_function/project_information.dart';
+import 'package:v2_1/project_space_screen/button_list/comfy_swipe_button.dart';
 import 'package:v2_1/project_space_screen/button_list/comfy_tap_button.dart';
 import 'package:v2_1/project_space_screen/button_list/comfy_toggle_button.dart';
 
@@ -15,6 +16,8 @@ class button_sort extends StatelessWidget {
           return comfy_tap_button(button: button);
         case 'toggle':
           return comfy_toggle_button(button: button);
+        case 'swipe':
+          return comfy_swipe_button(button: button);
         default:
           return Text('unidentified ${button.type}');
 
---
+++ v2_1/lib/project_space_screen/button_list/comfy_swipe_button.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../../home_screen/comfy_user_information_function/project_information.dart';
+import 'button_global/variables.dart';
+
+class comfy_swipe_button extends StatefulWidget {
+  const comfy_swipe_button({super.key, required this.button});
+  final comfy_button button;
+
+  @override
+  State<comfy_swipe_button> createState() => _comfy_swipe_buttonState();
+}
+
+class _comfy_swipe_buttonState extends State<comfy_swipe_button> {
+  bool _status = false;
+
+  void onClick(){
+    HapticFeedback.heavyImpact();
+    SystemSound.play(SystemSoundType.click);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: (){
+          onClick();
+          setState(
+                  () {
+                _status = !_status;
+              }
+          );
+        },
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 500),
+          child: Stack(
+              children: [
+                Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+
+
+                    )
+                ),
+                Padding(
+                  padding: _status?
+                  const EdgeInsets.only(
+                      top: 30, bottom: 30, left: 30, right: 30
+                  ): EdgeInsets.all(
+                      20
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          width: 2,
+                          color:Theme.of(context).colorScheme.onBackground
+                      ),
+                      borderRadius: button_border,
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).colorScheme.background,
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(6),
+                                  topRight: Radius.circular(6)
+                              )),
+                          child: Center(
+                            child: Text(
+                              widget.button.name!,
+                              style: Theme.of(context).textTheme.labelLarge,
+                            ),
+                          ),
+                        ),
+                        Container(height: 2, color: Colors.black,),
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: widget.button.color,
+                                borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(6),
+                                    bottomRight: Radius.circular(6)
+                                )
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+
+              ]
+          ),
+        )
+    );
+
+  }
+}
