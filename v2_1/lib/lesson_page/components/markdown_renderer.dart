c58c3299a42a5a27267b48529e6daec23eb45058
Thomas Nguyen <tungvunguyennguyen@gmail.com>
2024-04-29 18:50:08 +0700

    finalizing web
    setting up permission for storage on web is the key
    
    CORS:
    install gcloud
    gcloud auth login
    gcloud auth application-default login
    create a cors.json file
    gsutil cors set cors.json gs://storage_name.com

--- v2_1/lib/lesson_page/components/markdown_renderer.dart
+++ v2_1/lib/lesson_page/components/markdown_renderer.dart
@@ -1,7 +1,7 @@
 
 
-import 'package:dart_markdown/dart_markdown.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:dart_markdown/dart_markdown.dart' as md;
+import 'package:flutter_markdown/flutter_markdown.dart' as flmd;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
@@ -30,67 +30,72 @@ class _MarkdownRendererState extends State<MarkdownRenderer> {
             return randomLoadingWidget();
           }
           else if(snapshot.hasError == true){
+            //ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(snapshot.error.toString())));
             print('error loading markdown file ${snapshot.error.toString()}');
             return Center(
               child: Icon(Icons.error)
             );
           }
+
           else{
-            final markdown = Markdown(
-              // The options with default value `true`.
-              enableAtxHeading: true,
-              enableBlankLine: true,
-              enableBlockquote: true,
-              enableIndentedCodeBlock: true,
-              enableFencedBlockquote: true,
-              enableFencedCodeBlock: true,
-              enableList: true,
-              enableParagraph: true,
-              enableSetextHeading: true,
-              enableTable: true,
-              enableHtmlBlock: true,
-              enableLinkReferenceDefinition: true,
-              enableThematicBreak: true,
-              enableAutolinkExtension: true,
-              enableAutolink: true,
-              enableBackslashEscape: true,
-              enableCodeSpan: true,
-              enableEmoji: true,
-              enableEmphasis: true,
-              enableHardLineBreak: true,
-              enableImage: true,
-              enableLink: true,
-              enableRawHtml: true,
-              enableSoftLineBreak: true,
-              enableStrikethrough: true,
+              final markdown = md.Markdown(
+                // The options with default value `true`.
+                enableAtxHeading: true,
+                enableBlankLine: true,
+                enableBlockquote: true,
+                enableIndentedCodeBlock: true,
+                enableFencedBlockquote: true,
+                enableFencedCodeBlock: true,
+                enableList: true,
+                enableParagraph: true,
+                enableSetextHeading: true,
+                enableTable: true,
+                enableHtmlBlock: true,
+                enableLinkReferenceDefinition: true,
+                enableThematicBreak: true,
+                enableAutolinkExtension: true,
+                enableAutolink: true,
+                enableBackslashEscape: true,
+                enableCodeSpan: true,
+                enableEmoji: true,
+                enableEmphasis: true,
+                enableHardLineBreak: true,
+                enableImage: true,
+                enableLink: true,
+                enableRawHtml: true,
+                enableSoftLineBreak: true,
+                enableStrikethrough: true,
 
-              // The options with default value `false`.
-              enableHeadingId: false,
-              enableHighlight: false,
-              enableFootnote: false,
-              enableTaskList: false,
-              enableSubscript: false,
-              enableSuperscript: false,
-              enableKbd: false,
-              forceTightList: false,
+                // The options with default value `false`.
+                enableHeadingId: false,
+                enableHighlight: false,
+                enableFootnote: false,
+                enableTaskList: false,
+                enableSubscript: false,
+                enableSuperscript: false,
+                enableKbd: false,
+                forceTightList: false,
 
-              // Customised syntaxes.
-              extensions: const <Syntax>[],
-            );
-            print('snapshot data is ${snapshot.data.toString()}');
-            // AST nodes.
-            final nodes = markdown.parse(snapshot.data!);
+                // Customised syntaxes.
+                extensions: const <md.Syntax>[],
+              );
+              print('snapshot data is ${snapshot.data.toString()}');
+              // AST nodes.
+              final nodes = markdown.parse(snapshot.data!);
 
-            var html = nodes.toHtml(
-              enableTagfilter: false,
-              encodeHtml: true,
-            );
-            return Expanded(
-              child: HtmlWidget(
-                html,
-              )
-            );
+              var html = nodes.toHtml(
+                enableTagfilter: false,
+                encodeHtml: true,
+              );
+              return Center(
+                  child: HtmlWidget(
+                    html,
+                  )
+              );
+
+
           }
+
         }
     );
   }
